3dc335cb072d46a28e1fda88aee1c012c428572d
Huyhtq <huyyhuynh2325@gmail.com>
2025-04-11 13:50:57 +0700

    add example page ui

---
+++ src/main/java/com/invox/invoice_system/controller/MainController.java
@@ -0,0 +1,25 @@
+package com.invox.invoice_system.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+@RequestMapping("/invox")
+public class MainController {
+
+    @GetMapping("/")
+    public String redirectToHome() {
+        return "redirect:/invox/home";
+    }
+
+    @GetMapping("/home")
+    public String homePage() {
+        return "index";
+    }
+
+    
+}
